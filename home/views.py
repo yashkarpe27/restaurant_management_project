44d432af863646f6fc9309dc3588d0152f71e899
yashkarpe27 <yash55karpe@gmail.com>
2025-08-18 13:25:33 +0530

    28120

--- home/views.py
+++ home/views.py
@@ -9,6 +9,10 @@ def home(request):
     }
     return HttpResponse(template.render(context, request))
 
+def error(request):
+    template = loader.get_template('404.html')
+    return HttpResponse(template.render())
+
 def about(request):
     template = loader.get_template('about.html')
     return HttpResponse(template.render())
