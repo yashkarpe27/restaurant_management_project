150b87b444c4f8287ac5ed3a6a64d4e6420befb2
yashkarpe27 <yash55karpe@gmail.com>
2025-08-18 12:56:07 +0530

    28119

--- home/views.py
+++ home/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import HttpResponse
+from django.conf import settings
+from django.template import loader
 
-# Create your views here.
+def home(request):
+    template = loader.get_template('homepage.html')
+    context = {
+        'restaurant_name': settings.restaurant_name
+    }
+    return HttpResponse(template.render(context, request))
